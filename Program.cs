3502bfabae34416bc1a024d4f5077aa1fe4643e8
Nicky van Meurs <1681034+nvanmeurs@users.noreply.github.com>
2020-05-08 19:45:56 +0200

    Initial work to get an API response

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace TrelloParcelTracker
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter country, e.g. NL");
+            var country = Console.ReadLine();
+
+            Console.WriteLine("Enter postal code, e.g. 1234AB");
+            var postalCode = Console.ReadLine();
+
+            Console.WriteLine("Enter parcel barcode, e.g. 3SABCD428609");
+            var barcode = Console.ReadLine();
+
+            dynamic parcelStatus = await GetParcelStatus(country, postalCode, barcode);
+            Console.WriteLine($"Shipment status: {parcelStatus.shipmentPhase}");
+        }
+
+        private static async Task<object> GetParcelStatus(string country, string postalCode, string barcode)
+        {
+            var httpClient = new HttpClient();
+            var apiUrl = $"https://www.internationalparceltracking.com/api/shipment?barcode={barcode}&country={country}&language=en&postalCode={postalCode}";
+            var apiResponse = await httpClient.GetStringAsync(apiUrl);
+            var parcelStatus = JsonConvert.DeserializeObject(apiResponse);
+            return parcelStatus;
         }
     }
 }
